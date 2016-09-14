5f81a63d0e505824df1fe04986f56c5adee28d31
rinze <rinze.bruining@tno.nl>
2016-09-14 16:19:30 +0200

    Add helicopter circling the city

---
+++ Assets/Code/HeliHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeliHandler : MonoBehaviour {
+
+    private Vector3 position = new Vector3(0, 0, 0);
+    private int counter = 0;
+
+
+    // Use this for initialization
+    void Start () {
+        position = new Vector3(0, -0.2F, 4.0F);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        counter += 4;
+        updatePosition();
+    }
+
+    void updatePosition()
+    {
+        var body = this.GetComponent<Rigidbody>();
+        position.x = Mathf.Sin(Mathf.Deg2Rad * counter);
+        position.z = Mathf.Cos(Mathf.Deg2Rad * counter) + 4.0f;
+        body.transform.position = position;
+        body.transform.rotation = Quaternion.Euler(0, counter + 90, 0);
+    }
+}
