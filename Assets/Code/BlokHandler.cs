ef7c0c4a616b6fdec7c6bdc1b1bcce356ae0b50d
Arnoud <damylen@gmail.com>
2016-09-14 15:14:55 +0200

    refactoring code files locations

---
+++ Assets/Code/BlokHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlokHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+
+    public void Tap()
+    {
+        var s = 10;
+        var body = this.GetComponent<Rigidbody>();
+        //body.useGravity = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
