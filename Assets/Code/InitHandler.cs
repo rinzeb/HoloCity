0933c7f7f532805d0d3a571f3e165c977c917899
Arnoud <damylen@gmail.com>
2016-09-15 10:32:38 +0200

    Move methods to MeshHelpers

--- Assets/Code/InitHandler.cs
+++ Assets/Code/InitHandler.cs
@@ -114,72 +114,6 @@ public class InitHandler : MonoBehaviour {
     private Material _meshMaterial;
 
 
-
-    private void CreateMesh(List<Vector3> corners, float height, List<Vector3> verts, List<int> indices)
-    {
-        var tris = new Triangulator(corners);
-        var vertsStartCount = verts.Count;
-        verts.AddRange(corners.Select(x => new Vector3(x.x, height, x.z)).ToList());
-        indices.AddRange(tris.Triangulate().Select(x => vertsStartCount + x));
-
-        // if (typeSettings.IsVolumetric)
-        {
-
-            Vector3 v1;
-            Vector3 v2;
-            int ind = 0;
-            for (int i = 1; i < corners.Count; i++)
-            {
-                v1 = verts[vertsStartCount + i - 1];
-                v2 = verts[vertsStartCount + i];
-                ind = verts.Count;
-                verts.Add(v1);
-                verts.Add(v2);
-                verts.Add(new Vector3(v1.x, 0, v1.z));
-                verts.Add(new Vector3(v2.x, 0, v2.z));
-
-                indices.Add(ind);
-                indices.Add(ind + 2);
-                indices.Add(ind + 1);
-
-                indices.Add(ind + 1);
-                indices.Add(ind + 2);
-                indices.Add(ind + 3);
-            }
-
-            v1 = verts[vertsStartCount];
-            v2 = verts[vertsStartCount + corners.Count - 1];
-            ind = verts.Count;
-            verts.Add(v1);
-            verts.Add(v2);
-            verts.Add(new Vector3(v1.x, 0, v1.z));
-            verts.Add(new Vector3(v2.x, 0, v2.z));
-
-            indices.Add(ind);
-            indices.Add(ind + 1);
-            indices.Add(ind + 2);
-
-            indices.Add(ind + 1);
-            indices.Add(ind + 3);
-            indices.Add(ind + 2);
-        }
-    }
-
-    private void CreateGameObject(string name, List<Vector3> vertices, List<int> indices, GameObject main)
-    {
-        var go = new GameObject(name + " Building");
-        var mesh = go.AddComponent<MeshFilter>().mesh;
-        go.AddComponent<MeshRenderer>();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = indices.ToArray();
-        mesh.RecalculateNormals();
-        go.GetComponent<MeshRenderer>().material = _meshMaterial;// _settings.GetSettingsFor(kind).Material;
-        //go.transform.position += Vector3.up * Order;
-        go.transform.SetParent(main.transform, false);
-    }
-
-
-
     void InitVoiceCommands()
     {
 
@@ -246,8 +180,8 @@ public class InitHandler : MonoBehaviour {
             var verts = new List<Vector3>();
             var indices = new List<int>();
             var height = float.Parse(f.properties["gemiddelde_hoogte"].ToString());
-            CreateMesh(contour, height, verts, indices);
-            CreateGameObject("a" + i, verts, indices, main);
+            MeshHelpers.CreateMesh(contour, height, verts, indices);
+            MeshHelpers.CreateGameObject("a" + i, verts, indices, main, _meshMaterial);
 
         }
     }
